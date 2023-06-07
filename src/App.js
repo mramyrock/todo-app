35890daa3855aed58622bfe7b14a57553ab4b194
mramyrock <135337935+mramyrock@users.noreply.github.com>
2023-06-07 12:42:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import Todo from './TodoReact/Todo'
 import "../src/TodoReact/style.css"
-import Finput from './components/Finput'
 const App = () => {
 
   
   return (
     <div>
-    <Finput/>
       <Todo/>
     </div>
   )
